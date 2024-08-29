677b6516673eb3c8298f01984b5045c500c7c62a
Wim Bervoets <wim.bervoets@pxl.be>
2024-08-29 12:03:01 +0200

    init

---
+++ src/Popups/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Popups
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
