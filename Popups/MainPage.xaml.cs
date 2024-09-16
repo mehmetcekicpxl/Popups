ac7de89499ceb6c0009eb888110c117c750875c0
Wim Bervoets <wim.bervoets@pxl.be>
2024-09-16 19:23:18 +0200

    init

--- Popups/MainPage.xaml.cs
+++ Popups/MainPage.xaml.cs
@@ -8,34 +8,6 @@ namespace Popups
             InitializeComponent();
         }
 
-        private async void AddButton_Clicked(object sender, EventArgs e)
-        {
-            string student = await DisplayPromptAsync("Toevoegen", "Naam:");
-
-            if (!string.IsNullOrWhiteSpace(student))
-            {
-                Button btn = new Button
-                {
-                    Text = student,
-                    FontSize = 20,
-                    Padding=20,
-                    CornerRadius=10,
-                    BackgroundColor = GetRandomColor(),
-                };
-                btn.Clicked += Member_ClickedAsync;
-                flexLayout.Children.Add(btn);
-            }
-        }
-
-
-        private async void Member_ClickedAsync(object? sender, EventArgs e)
-        {
-            bool answer = await DisplayAlert("Verwijderen", $"Bent u zeker dat u {((Button)sender).Text} wil verwijderen?", "Ja", "Nee");
-
-            if (answer)
-                flexLayout.Children.Remove((Button)sender);
-        }
-
         private Random rnd = new Random(); 
         private Color GetRandomColor()
         {
