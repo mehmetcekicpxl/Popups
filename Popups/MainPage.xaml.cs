eca0798b63a5899a07a2514e84a5518f946e5348
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2025-10-01 20:55:09 +0200

    Pop-Ups

--- Popups/MainPage.xaml.cs
+++ Popups/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Tasks;
+
 namespace Popups
 {
     public partial class MainPage : ContentPage
@@ -12,6 +14,44 @@ namespace Popups
         private Color GetRandomColor()
         {
             return Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+
+        private async void ToevoegenBtn_Clicked(object sender, EventArgs e)
+        {
+            Button newButton = new Button();
+            newButton.Text = await DisplayPromptAsync("Add new Item", "wat is the name?");
+            newButton.Padding = 20;
+            newButton.FontSize = 20;
+            newButton.CornerRadius = 10;
+            newButton.BackgroundColor = GetRandomColor();
+
+            newButton.Clicked += ChangeNameClicked;
+            NamesFlexBox.Children.Add(newButton);
+
+        }
+        private async void ChangeNameClicked(object? sender,EventArgs e)
+        {
+            Button? clickedBtn = sender as Button;
+            if (clickedBtn is not null)
+            {
+
+                bool answer = await DisplayAlert("Verwijderen", $"Wil je '{clickedBtn.Text}' verwijdere?", "ja", "nee");
+
+                if (answer)
+                {
+                    NamesFlexBox.Children.Remove(clickedBtn);
+                }
+                else
+                {
+                    bool IsTheNameVeranderd = await DisplayAlert("Naam Veranderen", $"Wil je '{clickedBtn.Text}' veranderan?", "ja", "nee");
+                    if (IsTheNameVeranderd)
+                    {
+                        clickedBtn.Text = await DisplayPromptAsync("Add new Item", "wat is the name?");
+
+                        
+                    }
+                }
+            }
         }
     }
 
